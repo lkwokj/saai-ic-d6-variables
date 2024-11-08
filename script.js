16561f26a0b73b595696cb3221fe9c8c2e86456e
Lily Kwok <lkwokj@gmail.com>
2024-11-08 04:09:31 +0000

    added script file

---
+++ script.js
@@ -0,0 +1,30 @@
+let animal = "parrot"
+
+console.log("Awesome, I'm now writing Javascript!")
+console.log("I'm writing a 2nd line of JS")
+console.log(animal)// no reference for animal
+console.log("I'm still here...")
+
+//try it yourself
+//declare and console.log 3 variables
+//data types: string, number, boolean
+
+console.log("hi this is just an example")
+console.log("my parrot is 24 years old")
+console.log("I'm hungry")
+
+//string
+let personalName = "Lily"
+console.log(personalName)
+
+//number
+let purchasePrice = 14.97
+console.log("initial value purchasePrice", purchasePrice)
+
+purchasePrice = 9.99
+console.log("final value purchasePrice", purchasePrice)
+
+//boolean
+let isTuesday = false
+console.log(isTuesday)
+
